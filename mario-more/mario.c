700129fe2db5f568d78bce08e55bbe9be27fcbfe
EJDC <ewanconnor@gmail.com>
2022-07-24 11:19:02 +0000

    Sun, Jul 24, 2022, 11:19 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    int height;
+    do
+    {
+        height = get_height("Height:")
+    }
+    while (height < 8)
+
     
 }
