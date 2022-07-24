e5db16149979c0948d4c2e9cb1aecc7672be60b2
EJDC <ewanconnor@gmail.com>
2022-07-24 12:06:27 +0000

    Sun, Jul 24, 2022, 12:06 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -13,28 +13,21 @@ int main(void)
     }
     while (height > 8 || height < 1);
 
-    // creates rows based on height
+    // does this for appropruate number rows based on height
     for (int row = 0; row < height; row++)
     {
-        //create appropriate number of spaces on row
+        //prints appropriate number of spaces on row
         for (int spaces = 0; (spaces = 8 - height);)
             {
-            printf(" ");
+                printf(" ");
             }
 
-            //create appropriate number of bricks on row
+            //prints appropriate number of bricks on row
+            for (int bricks = 0; (bricks < height);)
+            {
+                printf("#");
+            }
 
-            for (int bricks = 0; (bricks < height;)
-
-        // creates columns based on height
-        for (int j = 0; j < height; j++)
-        {
-            //print brick
-            printf("#");
-        }
-
-        //next row
-        printf("\n");
     }
 
 
