577b9333aa2cf7dc3178c24253fff838f7a0800e
EJDC <ewanconnor@gmail.com>
2022-07-24 14:47:56 +0000

    Sun, Jul 24, 2022, 2:47 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    //user input height
+    //defines integer of height
     int height;
 
     //do while loop which continually prompts user for height which is an integer less than 8.
@@ -13,39 +13,32 @@ int main(void)
     }
     while (height > 8 || height < 1);
 
-    int bricks = 0;
+    int maxheight = 8
+    int spaces
 
     // does this for appropriate number rows based on height
     for (int row = 0; row < height; row++)
     {
-           //prints appropriate number of bricks on row
-         while (bricks < height)
+        //prints appropriate number of spaces on row
+        for (int spaces = 0; spaces < height ; spaces++)
             {
-                for()
-                bricks++;
-                printf("#");
-
-                // for (int spaces = 0; spaces < height ; spaces++)
-                // {
-                //     printf(" ");
-                // }
+                printf(" ");
             }
 
-
-        // prints appropriate number of spaces on row
-
-
-
-
+            //prints appropriate number of bricks on row
+            for (int bricks = 0; bricks < height; bricks++)
+            {
+                printf("#");
+            }
 
             //prints gap
                 printf("  ");
 
             //prints appropriate number of bricks on row
-            // for (int bricks = 0; bricks < height; bricks++)
-            // {
-            //     printf("#");
-            // }
+            for (int bricks = 0; bricks < height; bricks++)
+            {
+                printf("#");
+            }
 
             //creates new line
             printf("\n");
