870150f7642a682d8af46498995b40648f34633b
EJDC <ewanconnor@gmail.com>
2022-07-24 11:42:59 +0000

    Sun, Jul 24, 2022, 11:42 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -11,7 +11,7 @@ int main(void)
     {
         height = get_int("Height:");
     }
-    while (height > 8 || height < 0);
+    while (height > 8 || height < 1);
 
     // creates rows based on height
     for (int i = 0; i < height; i++)
