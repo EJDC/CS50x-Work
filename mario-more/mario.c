a977fc9a05dc3ecc0169a6b07e17c91acd0a577a
EJDC <ewanconnor@gmail.com>
2022-07-24 14:44:57 +0000

    Sun, Jul 24, 2022, 2:44 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -21,6 +21,7 @@ int main(void)
            //prints appropriate number of bricks on row
          while (bricks < height)
             {
+                for()
                 bricks++;
                 printf("#");
 
