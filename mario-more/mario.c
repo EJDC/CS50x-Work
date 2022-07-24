4ba6d6bd2cfc642528dc8714a469a7107689c0d4
EJDC <ewanconnor@gmail.com>
2022-07-24 11:58:13 +0000

    Sun, Jul 24, 2022, 11:58 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -14,11 +14,12 @@ int main(void)
     while (height > 8 || height < 1);
 
     // creates rows based on height
-    for (int row = 0; row < height; i++)
+    for (int row = 0; row < height; row++)
     {
-        for (int lspaces = 0; lspaces = 8 - height)
-        printf(" ")
-        
+        for (int lspaces = 0; (lspaces = 8 - height);)
+            {
+            printf(" ");
+            }
         // creates columns based on height
         for (int j = 0; j < height; j++)
         {
