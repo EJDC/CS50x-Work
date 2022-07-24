f86700fc3d04f91550c77b64a5e1d7b89fea7d2e
EJDC <ewanconnor@gmail.com>
2022-07-24 11:21:08 +0000

    Sun, Jul 24, 2022, 11:21 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,12 +3,15 @@
 
 int main(void)
 {
+    //defines integer of height
     int height;
+
+    //do while loop which continually prompts user for height which is an integer less than 8.
     do
     {
         height = get_height("Height:")
     }
     while (height < 8)
 
-    
+
 }
