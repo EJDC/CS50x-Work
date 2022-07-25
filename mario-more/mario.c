d2309bd919453bc2e71aad1c9283fec86e30b173
EJDC <ewanconnor@gmail.com>
2022-07-25 13:25:19 +0000

    Mon, Jul 25, 2022, 1:25 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -20,7 +20,7 @@ int main(void)
         {
            for (int j = -1; (j < i); j++)
             {
-             for (int k = 7; (j < k); k--)
+             for (int k = 0; (j < 1); k--)
                {
                  printf(".");
                }
