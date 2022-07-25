df6d2888377396336ca7fe25a9f2726e1be0dfba
EJDC <ewanconnor@gmail.com>
2022-07-25 10:54:46 +0000

    Mon, Jul 25, 2022, 10:54 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -20,7 +20,7 @@ int spaces = height -1;
 
     for (int i = 0; i < height; i++)
         {
-            for int j = 0; j
+            for int j = 0; j 
 
         //for each row, add a number of spaces = height-1
 
