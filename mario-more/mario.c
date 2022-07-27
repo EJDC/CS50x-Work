36fdc6a120bf9b1b30fe8bd99c2ecbc715363a02
EJDC <ewanconnor@gmail.com>
2022-07-27 10:51:46 +0000

    Wed, Jul 27, 2022, 10:51 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -21,7 +21,7 @@ int main(void)
 
           // nested loop - prints blocks
 
-             for (int k = 8; (k < i); k++)
+             for (int k = 8 - i; (k < height); k++)
                {
                  printf(".");
                 }
