c03927b2d7e3cb05c9b2907d2062927d010c24aa
EJDC <ewanconnor@gmail.com>
2022-07-28 09:25:46 +0000

    Thu, Jul 28, 2022, 9:25 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -26,6 +26,9 @@ int compute_score(string word)
     // TODO: Compute and return score for string
     for (int i = 0 , n = strlen(word), i < n, i++)
     {
-        if word[i] >= 'A' &&
+        if (word[i] >= 'A' && word[i] <= 'Z')
+        {
+
+        }
     }
 }
