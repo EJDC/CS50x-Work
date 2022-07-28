aad3f3d442b8e28e02160fe002d03eab69b05069
EJDC <ewanconnor@gmail.com>
2022-07-28 10:59:41 +0000

    Thu, Jul 28, 2022, 10:59 AM +00:00

--- scrabble/readability/readability.c
+++ scrabble/readability/readability.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_letters(string s)
+
 int main(void)
 {
     string s = get_string("Text: ");
+}
+
+int count_letters(string s)
+{
+    strlength(s)
 }
