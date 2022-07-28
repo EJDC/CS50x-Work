ac63181de3120597cd53971005ac73c65af17f85
EJDC <ewanconnor@gmail.com>
2022-07-28 11:07:43 +0000

    Thu, Jul 28, 2022, 11:07 AM +00:00

--- scrabble/readability/readability.c
+++ scrabble/readability/readability.c
@@ -1,15 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
-int count_letters(string s)
+int count_letters(string s);
 
 int main(void)
 {
     string s = get_string("Text: ");
+    printf(c);
 }
 
 int count_letters(string s)
 {
-    ifstrlength(s)
+    int c = 0;
+       for (int i = 0, n = strlen(s); i < n; i++)
+           if (isalpha(s[i]))
+           {
+            c += 1;
+           }
+
+        return c
+
 }
