6ea0110a779ca59a8ec56c7dad0b170a2284c966
EJDC <ewanconnor@gmail.com>
2022-07-28 12:34:30 +0000

    Thu, Jul 28, 2022, 12:34 PM +00:00

--- scrabble/readability/readability.c
+++ scrabble/readability/readability.c
@@ -24,7 +24,10 @@ int main(void)
     int sents = count_sentences(s);
     printf("%i sentences\n", sents);
 
-    
+float al = letters/100
+float as =
+float grade = 0.588 * al - 0.296 * as -15.8
+
 }
 
 int count_letters(string s)
