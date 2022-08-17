76704516a0acc4cbdb9fdeb106eebdec992f6fa7
EJDC <ewanconnor@gmail.com>
2022-08-17 11:05:49 +0000

    Wed, Aug 17, 2022, 11:05 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -133,7 +133,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
-            preferences[i][j] = (voter, rank);
+            preferences[i] = (voter, rank);
             return true;
         }
 
