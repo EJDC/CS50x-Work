7f1bd779e1076e3653cbcee57178c09dffeb82e0
EJDC <ewanconnor@gmail.com>
2022-08-18 08:58:11 +0000

    Thu, Aug 18, 2022, 8:58 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -174,8 +174,9 @@ bool print_winner(void)
 
     for(int i = 0; i < candidate_count; i++)
     {
-        int j = (candidates[i].votes / voter_count)*100;
-        if(j >= 50)
+   //     int j = (candidates[i].votes / voter_count)*100;
+    //    if(j >= 50)
+    if(candidates[i].votes > (voter_count/ 2))
         {
             printf("%s\n", candidates[i].name);
             return true;
