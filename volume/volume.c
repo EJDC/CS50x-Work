bf02125bbc2a2f84f8a56f0389bb0f4dea04af8d
EJDC <ewanconnor@gmail.com>
2022-08-23 14:43:56 +0000

    Tue, Aug 23, 2022, 2:43 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -4,8 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef uint8_t BYTE;
-
 // Number of bytes in .wav header
 const int HEADER_SIZE = 44;
 
@@ -36,16 +34,16 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    uint8_t header[HEADER_Size];
-    fread(header, HEADER_Size, 1, input);
-    fwrite(header, HEADER_Size, 1, output);
+    uint8_t header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
     while (fread(&buffer, sizeof(int16_t), 1, input))
     {
         buffer *= factor;
-        fwrite(&buffer, sizeof(int16_t, 1, output));
+        fwrite(&buffer, sizeof(int16_t), 1, output);
     }
 
     // Close files
