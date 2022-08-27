be991b998149301b6692a1c175bdad58bf7a70c6
EJDC <ewanconnor@gmail.com>
2022-08-27 08:30:59 +0000

    Sat, Aug 27, 2022, 8:30 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -19,6 +19,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = n;
+            image[i][j].rgbtBlue = n;
+            image[i][j].rgbtGreen =n;
+        }
+    }
     return;
 }
 
