44daf2d6acf5c22de529e1aa459326c6b8a4cd4f
EJDC <ewanconnor@gmail.com>
2022-08-27 09:27:08 +0000

    Sat, Aug 27, 2022, 9:27 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -62,6 +62,16 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int n = round((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen)/3.0);
+            image[i][j].rgbtRed = n;
+            image[i][j].rgbtBlue = n;
+            image[i][j].rgbtGreen =n;
+        }
+    }
     return;
 }
 
