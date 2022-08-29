3496a4da189f16debe220aa7fefc3666ff6d5ecf
EJDC <ewanconnor@gmail.com>
2022-08-29 10:01:03 +0000

    Mon, Aug 29, 2022, 10:01 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -92,6 +92,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    //store entire image in temp array, as pixel values in the original array will be changed as we iterate through.
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
