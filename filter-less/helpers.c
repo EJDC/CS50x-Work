1bf051bbe33672b677e29b74c682bd161bb60d73
EJDC <ewanconnor@gmail.com>
2022-08-29 09:56:29 +0000

    Mon, Aug 29, 2022, 9:56 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -92,5 +92,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //calculate rounded average
+            int n = round((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3.0);
+            image[i][j].rgbtRed = n;
+            image[i][j].rgbtBlue = n;
+            image[i][j].rgbtGreen = n;
+        }
+    }
     return;
 }
