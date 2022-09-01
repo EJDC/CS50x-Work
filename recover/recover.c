dcc71df21e48b1ef8dd57aaa67f76a5d13a4d40e
EJDC <ewanconnor@gmail.com>
2022-09-01 08:15:23 +0000

    Thu, Sep 1, 2022, 8:15 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -10,7 +10,27 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    
+    // Open file
+    FILE *file = fopen(argv[1], "r");
+    if (!file)
+    {
+        return 1;
+    }
+
+    // Read first three bytes
+    BYTE bytes[3];
+    fread(bytes, sizeof(BYTE), 3, file);
+
+    // Check first three bytes
+    if (bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff)
+    {
+        printf("Yes, possibly\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+
 
 
 }
