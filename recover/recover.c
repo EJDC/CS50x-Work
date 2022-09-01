3fd763f6e4ecd3a264dbf33f98bbbc72584b539d
EJDC <ewanconnor@gmail.com>
2022-09-01 08:14:33 +0000

    Thu, Sep 1, 2022, 8:14 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    //usage check - 1 command argument
+    if (argc != 2)
+    {
+        printf("Usage:  ./recover file.raw")
+        return 1;
+    }
+
+    
+
+
 }
