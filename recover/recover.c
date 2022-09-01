de47083376259a915d99bcffb3e430f63fb6b1b4
EJDC <ewanconnor@gmail.com>
2022-09-01 10:54:07 +0000

    Thu, Sep 1, 2022, 10:54 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
     char filename[8];
 
     //repeat until end of card
-    while (fread(buffer, 512, 1, file) == 512)
+    while (fread(buffer, 512, 1, file) == 1)
     {
     //if first bytes are a jpeg header
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
