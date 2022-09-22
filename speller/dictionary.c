7c4aa4672556aa5b8a7aa6addd44a986c23378d9
EJDC <ewanconnor@gmail.com>
2022-09-22 10:49:42 +0000

    Thu, Sep 22, 2022, 10:49 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -93,7 +93,7 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return wordcount;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
