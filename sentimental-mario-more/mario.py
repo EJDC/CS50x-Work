fdae603626e74c07f3cf7b28ff5e93f7b5550a1e
EJDC <ewanconnor@gmail.com>
2022-09-26 15:03:24 +0000

    Mon, Sep 26, 2022, 3:03 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,2 +1,17 @@
 # TODO
+def main():
+    height = get_height()
+    for i in range(height):
+        print("#")
 
+def get_height():
+    while True:
+        try:
+            n = int(input("Height: "))
+            if n > 0:
+                break
+        except ValueError:
+            print("That's not an integer!")
+    return n
+
+main()
