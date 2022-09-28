7704e54927d9cdc2cf952a9d38a6ad4e0578301b
EJDC <ewanconnor@gmail.com>
2022-09-28 13:06:26 +0000

    Wed, Sep 28, 2022, 1:06 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,7 +1,22 @@
 # TODO
 def main():
     cents = get_cents()
-    print(cents)
+    coins = 0
+    while cents > 0:
+        if cents >= 25:
+            cents = cents - 25
+            coins = coins + 1
+        elif cents >= 10:
+            cents = cents - 10
+            coins = coins + 1
+        elif cents >= 5:
+            cents = cents - 5
+            coins = coins + 1
+        elif cents >= 1:
+            cents = cents - 1
+            coins = coins + 1
+
+    print(coins)
 
 
 def get_cents():
