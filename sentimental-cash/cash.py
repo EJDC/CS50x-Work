211f27c39379c05584a364b0951944d4c0d7d22b
EJDC <ewanconnor@gmail.com>
2022-09-28 12:49:51 +0000

    Wed, Sep 28, 2022, 12:49 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,1 +1,12 @@
 # TODO
+def main():
+    cents = get_cents
+
+
+def get_cents()
+    while True:
+        try:
+            n = int(input("No. of Cents:")
+            if n > 0:
+                break
+    return n
