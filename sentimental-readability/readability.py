f875044e52a116758baa146baa382d318b5f0cfb
EJDC <ewanconnor@gmail.com>
2022-09-28 14:13:14 +0000

    Wed, Sep 28, 2022, 2:13 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,35 +1,35 @@
 # TODO
 
 while True:
-    #get input
+    # get input
     text = str(input("Text: "))
     length = len(text)
 
-    #start counters
+    # start counters
     letters = 0
     words = 1
     sentences = 0
 
-    #count letters
+    # count letters
     for i in range(length):
-        if(text[i].isalpha()):
+        if (text[i].isalpha()):
             letters += 1
-    #count words
+    # count words
         if (text[i].isspace()):
-            words +=1
+            words += 1
 
-    #count sentences
-        if(text[i] == '.' or text[i] == '?' or text[i] == '!'):
-            sentences +=1
+    # count sentences
+        if (text[i] == '.' or text[i] == '?' or text[i] == '!'):
+            sentences += 1
 
-    #Coleman-Liau calculation
-    calc = round((0.0588 * letters / words *100) - (0.296 * sentences / words *100) - 15.8)
+    # Coleman-Liau calculation
+    calc = round((0.0588 * letters / words * 100) - (0.296 * sentences / words * 100) - 15.8)
 
     if calc < 1:
-        print ("Before Grade 1")
+        print("Before Grade 1")
     elif calc > 16:
-        print ("Grade 16+")
+        print("Grade 16+")
     else:
-        print (f"Grade {calc}")
+        print(f"Grade {calc}")
 
     break
