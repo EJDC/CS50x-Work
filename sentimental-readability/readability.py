60bad15fe1f85cc2ca53fcf25aa8c0fbf3538770
EJDC <ewanconnor@gmail.com>
2022-09-28 13:26:16 +0000

    Wed, Sep 28, 2022, 1:26 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,15 @@
 # TODO
+
+
+
+
+def get_cents():
+    while True:
+        try:
+            n = float(input("No. of Cents: "))
+            if n > 0:
+                break
+        except ValueError:
+            print("That's not a valid number!")
+    return n
+
