3a619482761ab2e3df839b2528394fdcb7e6e603
EJDC <ewanconnor@gmail.com>
2023-05-07 17:45:01 +0000

    Sun, May 7, 2023, 5:45 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,6 @@
 SELECT title FROM movies
-JOIN stars ON stars.movie_id = movies.id
-JOIN people ON stars.person_id = people.id
-WHERE people.name = 'Johnny Depp' AND people.name = 'Helena Bonham Carter'
+JOIN stars star1 ON star1.movie_id = movies.id
+JOIN people person1 ON star1.person_id = person1.id
+JOIN stars star2 ON star2.movie_id = movies.id
+JOIN people person2 ON star2.person_id = person2.id
+WHERE person1.name = 'Johnny Depp' AND person2.name = 'Helena Bonham Carter'
