dd0049df1c3130d1222de008a315a0983ed587c8
EJDC <ewanconnor@gmail.com>
2023-05-07 19:12:57 +0000

    Sun, May 7, 2023, 7:12 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -48,6 +48,7 @@ JOIN passengers ON people.passport_number = passengers.passport_number
 JOIN flights ON passengers.flight_id = flights.id
 JOIN airports originair ON flights.origin_airport_id = airports.id
 JOIN airports destair ON flights.destination_airport_id = airports.id
+JOIN bakery_security_logs ON 
 WHERE atm_transactions.year = 2021
   AND atm_transactions.month = 7
   AND atm_transactions.day = 28
