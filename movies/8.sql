4eeb71001e337e2a7837268dc4b290a1bdf20a1a
EJDC <ewanconnor@gmail.com>
2023-05-07 17:07:27 +0000

    Sun, May 7, 2023, 5:07 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,4 +1,4 @@
 SELECT name FROM people
-JOIN people ON stars.person_id = people.id
+JOIN stars ON stars.person_id = people.id
 JOIN movies ON stars.movie_id = movie.id
 WHERE movies.name = 'Toy Story'
