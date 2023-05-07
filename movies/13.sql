4e4a1c1316b473fc1e5923551e1fa55e1f29f93d
EJDC <ewanconnor@gmail.com>
2023-05-07 17:50:27 +0000

    Sun, May 7, 2023, 5:50 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,7 @@
-SELECT name FROM people
-JOIN stars ON stars.person_id = people.id
+SELECT otherstar.name FROM people
 JOIN movies ON stars.movie_id = movies.id
+JOIN stars kevin ON kevin.movie_id = movies.id
+JOIN people kevin ON kevin.person_id = person1.id
+JOIN stars otherstar ON otherstar.movie_id = movies.id
+JOIN people otherstar ON otherstar.person_id = person2.id
+WHERE kevin.name = 'Kevin Bacon' AND kevin.birth = 1958
