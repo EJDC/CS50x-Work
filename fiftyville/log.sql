16f883a32a4cf4e38d61da8303bb472dc78758a3
EJDC <ewanconnor@gmail.com>
2023-05-07 19:20:33 +0000

    Sun, May 7, 2023, 7:20 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -73,5 +73,17 @@ WHERE atm_transactions.year = 2021
   AND duration < 60
   AND origin_airport = 'Fiftyville Regional Airport';
 
+SELECT name, bakery_security_logs.hour, bakery_security_logs.minute
+  FROM people
+  JOIN bakery_security_logs
+    ON people.license_plate = bakery_security_logs.license_plate
+ WHERE bakery_security_logs.year = 2021
+   AND bakery_security_logs.month = 7
+   AND bakery_security_logs.day = 28
+   AND bakery_security_logs.activity = 'exit'
+   AND bakery_security_logs.hour = 10
+   AND bakery_security_logs.minute >= 15
+   AND bakery_security_logs.minute <= 25
+ ORDER BY bakery_security_logs.minute;
 
 
