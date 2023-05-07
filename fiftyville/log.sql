ed3c373fe0560135eea05bf7922f57a951005186
EJDC <ewanconnor@gmail.com>
2023-05-07 18:36:00 +0000

    Sun, May 7, 2023, 6:36 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,6 +2,5 @@
 SELECT * FROM crime_scene_reports WHERE street = 'Humphrey Street' AND year = 2021 AND month = 7 AND day = 28;
 SELECT * FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 SELECT * FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';
-SELECT * FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw'
-JOIN bank_accounts ON bank_accounts.account_number = atm_transactions.account_number
+SELECT * FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw' JOIN bank_accounts ON bank_accounts.account_number = atm_transactions.account_number
 
