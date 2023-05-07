ebf6f1d10be90c7373f54eba5f958a34418c1528
EJDC <ewanconnor@gmail.com>
2023-05-07 13:58:18 +0000

    Sun, May 7, 2023, 1:58 PM +00:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,3 +1,3 @@
 SELECT * from SONGS
-JOIN ARTISTS on artist_id = artists.artist_id
-WHERE name = 'Post Malone';
+JOIN ARTISTS on artist.artist_id = artists.artist_id
+WHERE artists.name = 'Post Malone';
