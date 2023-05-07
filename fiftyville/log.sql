4df3e6af2adc751218ff51b2e480b203aef00caf
EJDC <ewanconnor@gmail.com>
2023-05-07 19:08:46 +0000

    Sun, May 7, 2023, 7:08 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -55,5 +55,21 @@ WHERE atm_transactions.year = 2021
   AND transaction_type = 'withdraw'
   AND duration < 60;
 
+SELECT  name, phone_number, license_plate, amount, originair.name as origin_airport, destair.name as destination_airport
+FROM atm_transactions
+JOIN bank_accounts ON bank_accounts.account_number = atm_transactions.account_number
+JOIN people ON bank_accounts.person_id = people.id
+JOIN phone_calls ON people.phone_number = phone_calls.caller
+JOIN passengers ON people.passport_number = passengers.passport_number
+JOIN flights ON passengers.flight_id = flights.id
+JOIN airports originair ON flights.origin_airport_id = originair.id
+JOIN airports destair ON flights.destination_airport_id = destair.id
+WHERE atm_transactions.year = 2021
+  AND atm_transactions.month = 7
+  AND atm_transactions.day = 28
+  AND atm_location = 'Leggett Street'
+  AND transaction_type = 'withdraw'
+  AND duration < 60;
+
 
 
