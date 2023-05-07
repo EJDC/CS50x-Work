eb60b42a8ad11eaf8d948d572c522443cb751e8d
EJDC <ewanconnor@gmail.com>
2023-05-07 18:32:03 +0000

    Sun, May 7, 2023, 6:32 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT * FROM crime_scene_reports WHERE street = 'Humphrey Street' AND year = 2021 AND month = 7 AND day = 28;
 SELECT * FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
+sqlite> D month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';
