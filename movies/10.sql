6f04b356538283cf9f9332778f5f5ba5a29110b5
EJDC <ewanconnor@gmail.com>
2023-05-07 17:19:14 +0000

    Sun, May 7, 2023, 5:19 PM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,5 +1,5 @@
 SELECT name FROM people
 JOIN directors ON directors.person_id = people.id
 JOIN movies ON directors.movie_id = movies.id
-JOIN ratings ON 
+JOIN ratings ON directors.movie_id = movies.id
 WHERE movies.rating >= 9.0
