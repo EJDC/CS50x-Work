465555cd7c700f80f93b9c7493eed550c51c7aa7
EJDC <ewanconnor@gmail.com>
2023-05-07 17:53:00 +0000

    Sun, May 7, 2023, 5:53 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,7 +1,7 @@
-SELECT DISTINCT otherstar.name FROM people
-JOIN movies ON stars.movie_id = movies.id
-JOIN stars kevin ON kevin.movie_id = movies.id
-JOIN people kevin ON kevin.person_id = person1.id
-JOIN stars otherstar ON otherstar.movie_id = movies.id
-JOIN people otherstar ON otherstar.person_id = person2.id
-WHERE kevin.name = 'Kevin Bacon' AND kevin.birth = 1958
+SELECT DISTINCT otherstar.name
+FROM people kevin
+JOIN stars kevin_stars ON kevin_stars.person_id = kevin.id
+JOIN movies ON movies.id = kevin_stars.movie_id
+JOIN stars other_stars ON other_stars.movie_id = movies.id
+JOIN people otherstar ON otherstar.id = other_stars.person_id
+WHERE kevin.name = 'Kevin Bacon' AND kevin.birth = 1958;
