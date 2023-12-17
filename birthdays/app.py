8275b8d1a5693c383c87a533414136302d0c46e8
Ewan Connor <ewanconnor@gmail.com>
2023-12-17 17:15:57 +0000

    Sun, Dec 17, 2023, 5:15 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -31,9 +31,7 @@ def index():
         name = request.form['name']
         month = request.form['month']
         day = request.form['day']
-        sql = "INSERT into birthdays (name, month, day) VALUES (?, ?, ?)"
-        values = [name, month, day]
-        results=db.execute(sql, values)
+        db.execute = ("INSERT into birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
         return redirect("/")
 
     else:
