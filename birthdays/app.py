c45f585ac427fd9f8b58d4fbd07a51474f9e3c2c
Ewan Connor <ewanconnor@gmail.com>
2023-12-17 16:24:44 +0000

    Sun, Dec 17, 2023, 4:24 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,7 +33,9 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-        
+        birthdays = []
+        sql ="SELECT * from birthdays"
+        results = 
 
         return render_template("index.html")
 
