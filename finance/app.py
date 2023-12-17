b7fcae4aae44288ff803b36abd54e1d4f6b6501b
Ewan Connor <ewanconnor@gmail.com>
2023-12-17 20:04:44 +0000

    Sun, Dec 17, 2023, 8:04 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -7,7 +7,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
 
-from helpers import apology, login_required, lookup, usd, getTime
+from helpers import apology, login_required, lookup, usd
 
 # Configure application
 app = Flask(__name__)
