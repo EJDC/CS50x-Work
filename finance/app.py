e738454f3ad8050375579544db8748b7b651a33a
Ewan Connor <ewanconnor@gmail.com>
2023-12-18 11:33:01 +0000

    Mon, Dec 18, 2023, 11:33 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -70,10 +70,10 @@ def calculate_portfolio_value(portfolio, cash):
 
     for item in portfolio:
         # Assuming you have a function to get the current price of a stock from an API
-        stock_price = lookup(item)
-        print(stock_price)
-        if stock_price is not None:
-            total_value += stock_price * int(item["no_of_shares"])
+        stock = lookup(item["symbol"])
+        print(stock)
+        if stock is not None:
+            total_value += stock["price"] * int(item["no_of_shares"])
 
     return total_value
 
